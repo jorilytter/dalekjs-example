28255f6654fe88c8d582968781d4f53c04f7cd87
jorilytter <jori.lytter@gmail.com>
2015-05-24 14:05:47 +0300

    Initialize example project for using dalek.js

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dalekjs-example
 Simple example project for using dalek.js
+
+# Install
+* prequisites
+ * npm installed
+
+```npm install```
+
+# Run tests
+
+* headless with phantom.js
+
+  ```./node_modules/dalek-cli/bin/cmd.js *.js```
+
+* with google chrome
+
+  ```./node_modules/dalek-cli/bin/cmd.js *.js -b chrome```
+
---
+++ github.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'GitHub example testing': function (test) {
+	test
+		.open('https://github.com')
+		.waitForElement('input[name=q]')
+		.assert.title().is('GitHub · Build software better, together.', 'GitHub has the correct page title!')
+		.type('input[name=q]', 'dalekjs')
+		.submit('.js-site-search-form')
+		.assert.text('.sort-bar > h3:nth-child(2)', 'We\'ve found 53 repository results')
+		.done();
+	}
+};

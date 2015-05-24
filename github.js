90c3f1c7124b7f1fef26bdd5eafa479108b2fc58
jorilytter <jori.lytter@gmail.com>
2015-05-24 14:36:23 +0300

    Fix indentation on js. Add execute example.

--- github.js
+++ github.js
@@ -1,12 +1,15 @@
 module.exports = {
-	'GitHub example testing': function (test) {
-	test
-		.open('https://github.com')
-		.waitForElement('input[name=q]')
-		.assert.title().is('GitHub · Build software better, together.', 'GitHub has the correct page title!')
-		.type('input[name=q]', 'dalekjs')
-		.submit('.js-site-search-form')
-		.assert.text('.sort-bar > h3:nth-child(2)', 'We\'ve found 53 repository results')
-		.done();
-	}
+  'GitHub example testing': function (test) {
+  test
+    .open('https://github.com')
+    .waitForElement('input[name=q]')
+    .assert.title().is('GitHub · Build software better, together.', 'GitHub has the correct page title!')
+    .type('input[name=q]', 'dalekjs')
+    .submit('.js-site-search-form')
+    .assert.text('.sort-bar > h3:nth-child(2)', 'We\'ve found 53 repository results')
+    .execute(function() {
+      return $('li.repo-list-item:nth-child(1) > h3:nth-child(2) > a:nth-child(1)').text() === 'dalekjs/dalek'
+    })
+    .done();
+  }
 };

addf7a7e917f1932076e28c16fde788f1d77592e
jorilytter <jori.lytter@gmail.com>
2015-05-24 14:48:18 +0300

    Change execution order within test

--- github.js
+++ github.js
@@ -6,10 +6,10 @@ module.exports = {
     .assert.title().is('GitHub · Build software better, together.', 'GitHub has the correct page title!')
     .type('input[name=q]', 'dalekjs')
     .submit('.js-site-search-form')
-    .assert.text('.sort-bar > h3:nth-child(2)', 'We\'ve found 53 repository results')
     .waitFor(function() {
       return $('li.repo-list-item:nth-child(1) > h3:nth-child(2) > a:nth-child(1)').text() === 'dalekjs/dalek'
     })
+    .assert.text('.sort-bar > h3:nth-child(2)', 'We\'ve found 53 repository results')
     .done();
   }
 };
